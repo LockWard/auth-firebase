b171fd717b2aac90a8bb71850f7b02ff2378a6ac
LockWard <roger1210@live.com>
2020-04-07 14:12:18 -0400

    Final

--- src/App.js
+++ src/App.js
@@ -7,35 +7,32 @@ import Home from './Home';
 
 class App extends Component{
   
-  constructor(props) {
-    super(props);
-    this.state = ({
-      user: null,
-    });
-    this.authListener = this.authListener.bind(this);
-  }
+    constructor(props) {
+      super(props);
+      this.state = ({
+        user: null,
+      });
+      this.authListener = this.authListener.bind(this);
+    }
 
-  componentDidMount(){
-    this.authListener();
-  }
+    componentDidMount() {
+      this.authListener();
+    }
 
-  authListener() {
-    fire.auth().onAuthStateChanged((user) => {
-      //console.log(user);
-      if (user) {
-        this.setState({user});
-        //localStorage.setItem('user', user.id);      
-      } else {
-        this.setState({user: null});
-        //localStorage.removeItem('user');
-      }
-    });
-  }
+    authListener() {
+      fire.auth().onAuthStateChanged((user) => {
+        if (user) {
+          this.setState({user});     
+        } else {
+          this.setState({user: null});
+        }
+      });
+    }
 
   render() {
     return (
       <div>
-        {this.state.user ? ( <Home />) : (<Login />)}
+          {this.state.user ? ( <Home />) : (<Login />)}
       </div>
     );
   }
--- src/Login.js
+++ src/Login.js
@@ -7,7 +7,7 @@ import cinema from './img/home_cinema.svg';
 
 class Login extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.login = this.login.bind(this);
         this.handleChange = this.handleChange.bind(this);
@@ -18,81 +18,56 @@ class Login extends Component {
         };
     }
 
-login (e) {
-    e.preventDefault();
-    fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u) =>{
-    }).then((u)=>{console.log(u)})
-    .catch((error) => {
-        console.log(error);
-      })
-  }
+    login(e) {
+        e.preventDefault();
+        fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u) =>{
+            }).then((u)=>{console.log(u)}).catch((error) => {console.log(error);})
+    }
 
-signup(e){
-    e.preventDefault();
-    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
-    }).then((u)=>{console.log(u)})
-    .catch((error) => {
-        console.log(error);
-      })
-  }
+    signup(e) {
+        e.preventDefault();
+        fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
+            }).then((u)=>{console.log(u)}).catch((error) => {console.log(error);})
+    }
 
-handleChange(e) {
-    this.setState({[e.target.name]: e.target.value});
-}
+    handleChange(e) {
+        this.setState({[e.target.name]: e.target.value});
+    }
 
-render() {
-    return(
-    /*<div className='col-md-6'>
-        <form>
-            <div class='form-group'>
-                <label for='exampleInputEmail1'>Email Addres</label>
-                <input value={this.state.email} onChange={this.handleChange} type='email' name='email'
-                className='form-control' id='exampleInputEmail1' aria-describedby='emailHelp' placeholder='Enter email'></input>
-                <small id='emailHelp' className='form-text text-muted'>We'll never share your email with anyone else.</small>
-            </div>    
-            <div class='form-group'>
-                <label for='exampleInputPassword1'>Password</label>
-                <input value={this.state.password} onChange={this.handleChange} type='password' name='password'
-                className='form-control' id='exampleInputPassword1' aria-describedby='emailHelp' placeholder='Enter pasword'></input>
-                <button type='submit' onClick={this.login} className='btn btn-primary'>Login</button>
-                <button onClick={this.signup} style={{marginLeft: '25px'}} className='btn btn-success'>Signup</button>
+    render() {
+        return(
+            <div className='container'>
+                <div className='img'>
+                    <img src={cinema} alt={'cinema'}></img>
+                </div>
+                <div className='login-content'>
+                <form>
+                    <img src={avatar} alt={'avatar'}></img>
+                    <h2 className='title'>Welcome</h2>
+                        <div className='input-div one'>
+                            <div className='i'>
+                                <i className='fas fa-user'></i>
+                            </div>
+                            <div className='div'>
+                                <input type='email' className='input' value={this.state.email} onChange={this.handleChange} name='email' id='exampleInputEmail1' aria-describedby='emailHelp' placeholder='Enter Email'></input>
+                            </div>                    
+                        </div>
+                        <div className='input-div pass'>
+                            <div className='i'>
+                                <i className='fas fa-lock'></i>
+                            </div>
+                            <div className='div'>
+                                <input type='password' className='input' value={this.state.password} onChange={this.handleChange} name='password' id='exampleInputPassword1' aria-describedby='emailHelp' placeholder='Enter Password'></input>
+                            </div>                    
+                        </div>
+                        <a href=''>Forgot Password?</a>
+                        <input type='submit' className='btn' value='Login' onClick={this.login}></input>
+                        <input type='submit' className='btn' value='Signup' onClick={this.signup}></input>
+                </form>
             </div>
-        </form>    
-    </div>*/
-    <div className='container'>
-        <div className='img'>
-            <img src={cinema} alt={'cinema'}></img>
         </div>
-        <div className='login-content'>
-            <form>
-                <img src={avatar} alt={'avatar'}></img>
-                <h2 className='title'>Welcome</h2>
-                <div className='input-div one'>
-                    <div className='i'>
-                        <i className='fas fa-user'></i>
-                    </div>
-                    <div className='div'>
-                        
-                        <input type='email' className='input' value={this.state.email} onChange={this.handleChange} name='email' id='exampleInputEmail1' aria-describedby='emailHelp' placeholder='Enter Email'></input>
-                    </div>                    
-                </div>
-                <div className='input-div pass'>
-                    <div className='i'>
-                        <i className='fas fa-lock'></i>
-                    </div>
-                    <div className='div'>
-                        
-                        <input type='password' className='input' value={this.state.password} onChange={this.handleChange} name='password' id='exampleInputPassword1' aria-describedby='emailHelp' placeholder='Enter Password'></input>
-                    </div>                    
-                </div>
-                <a href=''>Forgot Password?</a>
-                <input type='submit' className='btn' value='Login' onClick={this.login}></input>
-                <input type='submit' className='btn' value='Signup' onClick={this.signup}></input>
-            </form>
-        </div>
-    </div>
-    );
-}
-}
+            );
+        }
+    }
 
 export default Login;
